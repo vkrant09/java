215943aa8b8aabf48d9cb3f8612b97b15065cf98
Vikrant Singh <vp2620777@gmail.com>
2023-07-23 01:36:37 +0530

    new

---
+++ tu1.java
@@ -0,0 +1,7 @@
+class tu1{
+    public static void main(String[] args) {
+        String name ="Vikrant";
+        int a=10;
+       System.out.println(name+a);
+    }
+}
---
+++ tu2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+
+
+class tu2{
+
+    public static void main(String[] args) {
+   Scanner sc= new Scanner(System.in);
+   int button=sc.nextInt();
+//    if (button==1){
+//     System.out.println("nikll");
+
+//    }
+//    else if(button==2){
+//     System.out.println("aaja");
+
+//    }
+//    else
+//    System.out.println("baad");
+
+switch(button){
+    case 1:
+    System.out.println("heel");
+    break;
+    case 2: System.out.println("hool");
+    break;
+    case 3: System.out.println("new");
+    break;
+    default: System.out.println("invalid");
+}
+
+        
+    }
+}
